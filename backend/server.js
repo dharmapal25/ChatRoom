51feabd4308da025120753d59122ccc29c83c59c
dharmapal25 <dharmapalbharati2525@gmail.com>
2026-04-06 16:43:44 +0530

    second commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const app = require('./app');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT;
+
+// Connect to MongoDB
+connectDB();
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
